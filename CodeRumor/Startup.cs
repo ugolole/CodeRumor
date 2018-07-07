d7520f6213645565972ded82d42a512a47fa75e8
ugolole <ugololeivan@yahoo.com>
2018-07-07 04:35:05 -0400

    properly configured web site

--- CodeRumor/Startup.cs
+++ CodeRumor/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using static Microsoft.AspNetCore.Hosting.Internal.HostingApplication;
 
 namespace CodeRumor
 {
@@ -24,14 +25,6 @@ namespace CodeRumor
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -48,9 +41,10 @@ namespace CodeRumor
                 app.UseHsts();
             }
 
+
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
 
             app.UseMvc(routes =>
             {
@@ -59,5 +53,7 @@ namespace CodeRumor
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
+
+       
     }
 }
