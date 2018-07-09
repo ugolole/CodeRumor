2a4d33b9bbe1cf14743afffd4d827f54d3766ae5
ugolole <ugololeivan@yahoo.com>
2018-07-08 20:11:27 -0400

    Adding home page design

--- CodeRumor/wwwroot/js/site.js
+++ CodeRumor/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navBar").style.top = "0";
+    } else {
+        document.getElementById("navBar").style.top = "-100px";
+    }
+    prevScrollpos = currentScrollPos;
+}
