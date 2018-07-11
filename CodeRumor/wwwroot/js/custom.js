034d9b5b87e3e050a939f7f9dd7aecc2f88b14ec
ugolole <ugololeivan@yahoo.com>
2018-07-10 21:50:30 -0400

    Layout update

--- CodeRumor/wwwroot/js/custom.js
+++ CodeRumor/wwwroot/js/custom.js
@@ -9,13 +9,14 @@ var windowWidth = $(document).height();
 //$(document).width();
 
 var prevScrollpos = window.pageYOffset;
+var value = false;
 window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
 
         if (prevScrollpos > currentScrollPos) {
-            document.getElementById("navBar").style.top = "0";
+            document.getElementById("navBarxx").style.top = "0";
         } else {
-            document.getElementById("navBar").style.top = "-100px";
+            document.getElementById("navBarxx").style.top = "-100px";
         }
         prevScrollpos = currentScrollPos;
 
