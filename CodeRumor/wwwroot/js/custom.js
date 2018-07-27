43c40423454fc8a613a71b9a6ed813e9636ef079
ugolole <ugololeivan@yahoo.com>
2018-07-26 20:31:56 -0400

    First documentation release

--- CodeRumor/wwwroot/js/custom.js
+++ CodeRumor/wwwroot/js/custom.js
@@ -18,6 +18,149 @@ $(document).ready(function () {
         },
             'slow');
     });
+
+    //**************************C# documentation smooth scrolling 
+    //one
+    $('a[name="DOCHeader_menu1"]').click(function () {
+        $('html, body').animate({
+            scrollTop: $('a[name="DOCHeader1"]').offset().top - 50
+        }, 'slow');
+        //add the active class to show where we are
+        $('a[name="DOCHeader_menu1"]').addClass('active'); 
+
+        //remove other active members
+        $('a[name="DOCHeader_menu2"]').removeClass('active');
+        $('a[name="DOCHeader_menu3"]').removeClass('active');
+        $('a[name="DOCHeader_menu4"]').removeClass('active');
+        $('a[name="DOCHeader_menu5"]').removeClass('active');
+        $('a[name="DOCHeader_menu6"]').removeClass('active');
+        $('a[name="DOCHeader_menu7"]').removeClass('active');
+        $('a[name="DOCHeader_menu8"]').removeClass('active');
+
+    });
+
+    //two
+    $('a[name="DOCHeader_menu2"]').click(function () {
+        $('html, body').animate({
+            scrollTop: $('a[name="DOCHeader2"]').offset().top - 50
+        }, 'slow');
+        //add the active class to show where we are
+        $('a[name="DOCHeader_menu2"]').addClass('active');
+
+        //remove other active members
+        $('a[name="DOCHeader_menu1"]').removeClass('active');
+        $('a[name="DOCHeader_menu3"]').removeClass('active');
+        $('a[name="DOCHeader_menu4"]').removeClass('active');
+        $('a[name="DOCHeader_menu5"]').removeClass('active');
+        $('a[name="DOCHeader_menu6"]').removeClass('active');
+        $('a[name="DOCHeader_menu7"]').removeClass('active');
+        $('a[name="DOCHeader_menu8"]').removeClass('active');
+    });
+
+    //three
+    $('a[name="DOCHeader_menu3"]').click(function () {
+        $('html, body').animate({
+            scrollTop: $('a[name="DOCHeader3"]').offset().top - 50
+        }, 'slow');
+        //add the active class to show where we are
+        $('a[name="DOCHeader_menu3"]').addClass('active');
+
+        //remove other active members
+        $('a[name="DOCHeader_menu2"]').removeClass('active');
+        $('a[name="DOCHeader_menu1"]').removeClass('active');
+        $('a[name="DOCHeader_menu4"]').removeClass('active');
+        $('a[name="DOCHeader_menu5"]').removeClass('active');
+        $('a[name="DOCHeader_menu6"]').removeClass('active');
+        $('a[name="DOCHeader_menu7"]').removeClass('active');
+        $('a[name="DOCHeader_menu8"]').removeClass('active');
+    });
+
+    //four
+    $('a[name="DOCHeader_menu4"]').click(function () {
+        $('html, body').animate({
+            scrollTop: $('a[name="DOCHeader4"]').offset().top - 50
+        }, 'slow');
+        //add the active class to show where we are
+        $('a[name="DOCHeader_menu4"]').addClass('active');
+
+        //remove other active members
+        $('a[name="DOCHeader_menu2"]').removeClass('active');
+        $('a[name="DOCHeader_menu3"]').removeClass('active');
+        $('a[name="DOCHeader_menu1"]').removeClass('active');
+        $('a[name="DOCHeader_menu5"]').removeClass('active');
+        $('a[name="DOCHeader_menu6"]').removeClass('active');
+        $('a[name="DOCHeader_menu7"]').removeClass('active');
+        $('a[name="DOCHeader_menu8"]').removeClass('active');
+    });
+
+    $('a[name="DOCHeader_menu5"]').click(function () {
+        $('html, body').animate({
+            scrollTop: $('a[name="DOCHeader5"]').offset().top - 50
+        }, 'slow');
+        //add the active class to show where we are
+        $('a[name="DOCHeader_menu5"]').addClass('active');
+
+        //remove other active members
+        $('a[name="DOCHeader_menu1"]').removeClass('active');
+        $('a[name="DOCHeader_menu2"]').removeClass('active');
+        $('a[name="DOCHeader_menu3"]').removeClass('active');
+        $('a[name="DOCHeader_menu4"]').removeClass('active');
+        $('a[name="DOCHeader_menu6"]').removeClass('active');
+        $('a[name="DOCHeader_menu7"]').removeClass('active');
+        $('a[name="DOCHeader_menu8"]').removeClass('active');
+    });
+
+    $('a[name="DOCHeader_menu6"]').click(function () {
+        $('html, body').animate({
+            scrollTop: $('a[name="DOCHeader6"]').offset().top - 50
+        }, 'slow');
+        //add the active class to show where we are
+        $('a[name="DOCHeader_menu6"]').addClass('active');
+
+        //remove other active members
+        $('a[name="DOCHeader_menu1"]').removeClass('active');
+        $('a[name="DOCHeader_menu2"]').removeClass('active');
+        $('a[name="DOCHeader_menu3"]').removeClass('active');
+        $('a[name="DOCHeader_menu4"]').removeClass('active');
+        $('a[name="DOCHeader_menu5"]').removeClass('active');
+        $('a[name="DOCHeader_menu7"]').removeClass('active');
+        $('a[name="DOCHeader_menu8"]').removeClass('active');
+    });
+
+    $('a[name="DOCHeader_menu7"]').click(function () {
+        $('html, body').animate({
+            scrollTop: $('a[name="DOCHeader7"]').offset().top - 50
+        }, 'slow');
+        //add the active class to show where we are
+        $('a[name="DOCHeader_menu7"]').addClass('active');
+
+        //remove other active members
+        $('a[name="DOCHeader_menu1"]').removeClass('active');
+        $('a[name="DOCHeader_menu2"]').removeClass('active');
+        $('a[name="DOCHeader_menu3"]').removeClass('active');
+        $('a[name="DOCHeader_menu4"]').removeClass('active');
+        $('a[name="DOCHeader_menu5"]').removeClass('active');
+        $('a[name="DOCHeader_menu6"]').removeClass('active');
+        $('a[name="DOCHeader_menu8"]').removeClass('active');
+    });
+
+    $('a[name="DOCHeader_menu8"]').click(function () {
+        $('html, body').animate({
+            scrollTop: $('a[name="DOCHeader8"]').offset().top - 50
+        }, 'slow');
+        //add the active class to show where we are
+        $('a[name="DOCHeader_menu8"]').addClass('active');
+
+        //remove other active members
+        $('a[name="DOCHeader_menu1"]').removeClass('active');
+        $('a[name="DOCHeader_menu2"]').removeClass('active');
+        $('a[name="DOCHeader_menu3"]').removeClass('active');
+        $('a[name="DOCHeader_menu4"]').removeClass('active');
+        $('a[name="DOCHeader_menu5"]').removeClass('active');
+        $('a[name="DOCHeader_menu6"]').removeClass('active');
+        $('a[name="DOCHeader_menu7"]').removeClass('active');
+    });
+
 });
 
 
