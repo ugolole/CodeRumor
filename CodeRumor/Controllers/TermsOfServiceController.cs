81109074d6374af31c9c4b1e67cc6e6863a36e51
ugolole <ugololeivan@yahoo.com>
2018-09-22 00:07:40 -0400

    Upgrade to the footer

---
+++ CodeRumor/Controllers/TermsOfServiceController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CodeRumor.Models;
+
+namespace CodeRumor.Controllers
+{
+    public class TermsOfServiceController : Controller
+    {
+        public IActionResult TermsOfService()
+        {
+            NavigationIndicator active = new NavigationIndicator();
+
+            return View(active);
+        }
+    }
+}
--- CodeRumor/wwwroot/js/custom.js
+++ CodeRumor/wwwroot/js/custom.js
@@ -115,6 +115,150 @@ $(document).ready(function () {
         //add the active class to show where we are
         $(this).addClass('active'); 
     });
+
+
+    /****************************************************************************/
+    //**************************Terms of Service smooth scrolling 
+    //one
+    $('a[name="TERMsHeader_menu1"]').click(function () {
+        $('html, body').animate({
+            scrollTop: $('a[name="TERMsHeader1"]').offset().top - 50
+        }, 'slow');
+
+        //remove other active members
+        $('li a.menu').removeClass('active');
+
+        //add the active class to show where we are
+        $(this).addClass('active');
+
+    });
+
+
+    //two
+    $('a[name="TERMsHeader_menu2"]').click(function () {
+        $('html, body').animate({
+            scrollTop: $('a[name="TERMsHeader2"]').offset().top - 50
+        }, 'slow');
+        //remove other active members
+        $('li a.menu').removeClass('active');
+
+        //add the active class to show where we are
+        $(this).addClass('active');
+    });
+
+    //three
+    $('a[name="TERMsHeader_menu3"]').click(function () {
+        $('html, body').animate({
+            scrollTop: $('a[name="TERMsHeader3"]').offset().top - 50
+        }, 'slow');
+        //remove other active members
+        $('li a.menu').removeClass('active');
+
+        //add the active class to show where we are
+        $(this).addClass('active');
+    });
+
+    //four
+    $('a[name="TERMsHeader_menu4"]').click(function () {
+        $('html, body').animate({
+            scrollTop: $('a[name="TERMsHeader4"]').offset().top - 50
+        }, 'slow');
+        //remove other active members
+        $('li a.menu').removeClass('active');
+
+        //add the active class to show where we are
+        $(this).addClass('active');
+    });
+
+    $('a[name="TERMsHeader_menu5"]').click(function () {
+        $('html, body').animate({
+            scrollTop: $('a[name="TERMsHeader5"]').offset().top - 50
+        }, 'slow');
+        //remove other active members
+        $('li a.menu').removeClass('active');
+
+        //add the active class to show where we are
+        $(this).addClass('active');
+    });
+
+    $('a[name="TERMsHeader_menu6"]').click(function () {
+        $('html, body').animate({
+            scrollTop: $('a[name="TERMsHeader6"]').offset().top - 50
+        }, 'slow');
+        //remove other active members
+        $('li a.menu').removeClass('active');
+
+        //add the active class to show where we are
+        $(this).addClass('active');
+    });
+
+    $('a[name="TERMsHeader_menu7"]').click(function () {
+        $('html, body').animate({
+            scrollTop: $('a[name="TERMsHeader7"]').offset().top - 50
+        }, 'slow');
+        //remove other active members
+        $('li a.menu').removeClass('active');
+
+        //add the active class to show where we are
+        $(this).addClass('active');
+    });
+
+    $('a[name="TERMsHeader_menu8"]').click(function () {
+        $('html, body').animate({
+            scrollTop: $('a[name="TERMsHeader8"]').offset().top - 50
+        }, 'slow');
+        //remove other active members
+        $('li a.menu').removeClass('active');
+
+        //add the active class to show where we are
+        $(this).addClass('active');
+    });
+
+    $('a[name="TERMsHeader_menu9"]').click(function () {
+        $('html, body').animate({
+            scrollTop: $('a[name="TERMsHeader9"]').offset().top - 50
+        }, 'slow');
+        //remove other active members
+        $('li a.menu').removeClass('active');
+
+        //add the active class to show where we are
+        $(this).addClass('active');
+    });
+
+    $('a[name="TERMsHeader_menu10"]').click(function () {
+        $('html, body').animate({
+            scrollTop: $('a[name="TERMsHeader10"]').offset().top - 50
+        }, 'slow');
+        //remove other active members
+        $('li a.menu').removeClass('active');
+
+        //add the active class to show where we are
+        $(this).addClass('active');
+    });
+
+    $('a[name="TERMsHeader_menu11"]').click(function () {
+        $('html, body').animate({
+            scrollTop: $('a[name="TERMsHeader11"]').offset().top - 50
+        }, 'slow');
+        //remove other active members
+        $('li a.menu').removeClass('active');
+
+        //add the active class to show where we are
+        $(this).addClass('active');
+    });
+
+    $('a[name="TERMsHeader_menu12"]').click(function () {
+        $('html, body').animate({
+            scrollTop: $('a[name="TERMsHeader12"]').offset().top - 50
+        }, 'slow');
+        //remove other active members
+        $('li a.menu').removeClass('active');
+
+        //add the active class to show where we are
+        $(this).addClass('active');
+    });
+
+
    
 });
 
